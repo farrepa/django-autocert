edb7a7f00ef111213e14a90875e830e75f90ea72
Patrick Farrell <p@farrell.io>
2016-12-05 16:11:30 +0000

    Include migrations and bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(name='django-autocert',
-      version='0.1',
+      version='0.1.1',
       packages=['autocert'],
       include_package_data=True,
       license='MIT',
