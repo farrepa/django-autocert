6388ac19c7000e6ef0e6439c23fcf7df3a51b4c8
farrepa <p@farrell.io>
2018-02-01 17:07:41 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(name='django-autocert',
-      version='0.1.4',
+      version='0.1.5',
       packages=['autocert'],
       include_package_data=True,
       license='MIT',
